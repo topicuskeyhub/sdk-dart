3de9e8376a32d890d887a64fb603a207659abf15
Emond Papegaaij <emond.papegaaij@topicus.nl>
2025-10-27 13:35:43 +0100

    Upgrade dependencies

--- generate.sh
+++ generate.sh
@@ -2,7 +2,7 @@
 
 mkdir -p tmp
 cd tmp
-wget https://github.com/microsoft/kiota/releases/download/v1.28.0/linux-x64.zip
+wget https://github.com/microsoft/kiota/releases/download/v1.29.0/linux-x64.zip
 unzip linux-x64.zip
 cd ../lib
 find . -type f -name "*.dart" -print0 | xargs -0 grep -Z -l "auto generated" | xargs -0 rm -f
